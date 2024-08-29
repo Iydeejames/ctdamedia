958ef11b8a8c9695af181c1db2a1f283e1ea9d9b
Idaraobong James <iydeejaymes@gmail.com>
2024-08-29 03:11:05 +0100

    Bug fix

--- src/components/molecule/Navbar.jsx
+++ src/components/molecule/Navbar.jsx
@@ -1,17 +1,23 @@
-
+import DroneLogo from "../../assets/logo/Drone-logo.png";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <header className="bg-gray-800 w-full p-4">
+    <header className="bg-white w-full p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-white">Dr Drone</h1>
+        <div className="flex items-center space-x-2">
+          <img
+            src={DroneLogo}
+            alt="Drone Logo"
+            className="h-20 w-26 object-contain"
+          />
+        </div>
         <nav>
           <ul className="flex space-x-6">
-            <li><Link to="/" className="text-white hover:text-blue-400 transition duration-300">Home</Link></li>
-            <li><Link to="/features" className="text-white hover:text-blue-400 transition duration-300">Features</Link></li>
-            <li><Link to="/pricing" className="text-white hover:text-blue-400 transition duration-300">Pricing</Link></li>
-            <li><Link to="/contact" className="text-white hover:text-blue-400 transition duration-300">Contact</Link></li>
+            <li><Link to="/" className="text-gray-800 hover:text-blue-400 transition duration-300">Home</Link></li>
+            <li><Link to="/features" className="text-gray-800 hover:text-blue-400 transition duration-300">Features</Link></li>
+            <li><Link to="/pricing" className="text-gray-800 hover:text-blue-400 transition duration-300">Pricing</Link></li>
+            <li><Link to="/contact" className="text-gray-800 hover:text-blue-400 transition duration-300">Contact</Link></li>
           </ul>
         </nav>
       </div>
