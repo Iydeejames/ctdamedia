4ad43d06339793cebd5954a58ee669344fd5256e
Idaraobong James <iydeejaymes@gmail.com>
2024-12-06 16:16:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scrollLeft: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'scroll-left': 'scrollLeft 20s linear infinite',
+      },
+    },
   },
   plugins: [],
-}
+};
