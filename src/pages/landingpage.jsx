5c0aec4922c5d2ad9a4124c761ba47e2668c7eac
Idaraobong James <iydeejaymes@gmail.com>
2024-12-16 15:53:25 +0100

    Update landingpage.jsx

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -190,12 +190,13 @@ const LandingPage = () => {
       <ImageSliderSection />
       
       {/* Features Section */}
-      <ScrollSection>
       <section className="py-16 bg-gray-100">
         <div className="text-center">
+        <ScrollSection>
           <h2 className="text-2xl lg:text-3xl font-bold mb-12">
             Why Choose Dr. Drone Nig Ltd
           </h2>
+          </ScrollSection>
           <div
             className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 py-4"
             style={{
@@ -312,7 +313,7 @@ const LandingPage = () => {
           </div>
         </div>
       </section>
-      </ScrollSection>
+
 
 
 
