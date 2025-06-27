f1a04a8c84d7abb71be3b59824a0b7a0e8cbf702
Idaraobong James <iydeejaymes@gmail.com>
2025-06-27 17:59:08 +0100

    update

--- src/pages/main/LandingPage.jsx
+++ src/pages/main/LandingPage.jsx
@@ -183,7 +183,7 @@ const handleSubscribe = () => {
 {/* Mobile View: New UI Design */}
 <section className="lg:hidden px-0 mt-0  space-y-6">
       {/* Main Hero Image */}
-      <div className="lg:w-3/4 relative rounded overflow-hidden h-[240px] ">
+      <div className="lg:w-3/4 relative rounded overflow-hidden h-[260px] ">
   <img
     src={img4}
     alt="CTDA Media"
