ae9502fd0ffd0638ff1dffba7ef7a3ddca9e3ae4
Idaraobong James <iydeejaymes@gmail.com>
2025-07-05 15:43:32 +0100

    update

--- src/pages/main/LandingPage.jsx
+++ src/pages/main/LandingPage.jsx
@@ -324,7 +324,7 @@ const renderSection = (title, data, isGrid) => (
 {/* Mobile View: New UI Design */}
 <section className="lg:hidden px-0 mt-0  space-y-6">
       {/* Main Hero Image */}
-      <div className="lg:w-3/4 relative rounded overflow-hidden h-[550px] ">
+      <div className="lg:w-3/4 relative  overflow-hidden h-[550px] ">
   <img
     src={img4}
     alt="CTDA Media"
